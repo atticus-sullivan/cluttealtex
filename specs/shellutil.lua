72d1153230dc8810ad8ddb34653c9344048951bf
Lukas <oss.heindl+git@protonmail.com>
2024-12-19 22:54:58 +0100

    move the shellutil_unix tests to the specs/ folder and lester

--- specs/shellutil.lua
+++ specs/shellutil.lua
@@ -7,13 +7,20 @@ local expect   = lester.expect
 local shellutil_unix = require 'src_lua.texrunner.shellutil_unix'
 local shellutil_windows = require 'src_lua.texrunner.shellutil_windows'
 
--- describe('pathutil_unix', function()
--- 	describe('joinpath', function()
--- 		it('/path/to/somewhere', function()
--- 			expect.equal(pathutil_unix.join("/path/", "to", "somewhere"), "/path/to/somewhere")
--- 		end)
--- 	end)
--- end)
+describe('pathutil_unix', function()
+	describe('escape', function()
+		it([[Hello world!]], function()
+			expect.equal(shellutil_unix.escape([[Hello world!]]), [['Hello world!']])
+		end)
+		it([[Hello' world!]], function()
+			expect.equal(shellutil_unix.escape([[Hello' world!]]), [['Hello'"'"' world!']])
+		end)
+		it([[Hello' world!"]], function()
+			expect.equal(shellutil_unix.escape([[Hello' world!"]]), [['Hello'"'"' world!"']])
+		end)
+	end)
+end)
+
 
 describe('shellutil_windows', function()
 	describe('escape', function()
